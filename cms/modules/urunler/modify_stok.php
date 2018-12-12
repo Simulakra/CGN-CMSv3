d2af89918e9a539241eb248e215193b9db960f70
Berke D <tr.berkedundar@gmail.com>
2018-12-12 23:42:44 +0300

    gün sonu

--- cms/modules/urunler/modify_stok.php
+++ cms/modules/urunler/modify_stok.php
@@ -13,9 +13,25 @@ error_reporting(0);
 	<div class="panel panel-default">
 		<div class="panel-body">
 
-
+			<table class="table">
+				<tr>
+					<td>
+						<div class="col-6">
+							<label>Miktar:</label>
+							<input class="form-control" type="" name="">
+						</div>
+					</td>
+					<td>
+						<div class="col-6">
+							<label>Ürün Barkodu:</label>
+							<input class="form-control" type="" name="">
+						</div>
+					</td>
+				</tr>
+			</table>
 			<div class="col-12">
-				
+				<a class="btn btn-primary btn-default " href=""><i class="fa fa-plus"></i> Stok Giriş</a>
+				<a class="btn btn-primary btn-default " href=""><i class="fa fa-minus"></i> Stok Çıkış</a>
 			</div>
 
 
