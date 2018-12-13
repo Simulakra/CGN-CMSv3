4e1a3b1cfbbb61a277fa623c6d8473958291ca6e
Berke D <tr.berkedundar@gmail.com>
2018-12-13 16:33:45 +0300

    stok alarm bildirimi

--- cms/modules/urunler/product_main.php
+++ cms/modules/urunler/product_main.php
@@ -8,7 +8,9 @@ error_reporting(0);
 
 <div id="page-wrapper">
 <div class="row">
-<div class="col-lg-12"><h1 class="page-header">Ürünler<a class="btn btn-primary btn-default" href="modify_stok.php" style="float: right;"><i class="fa fa-dropbox fa-lg"aria-hidden='true'></i> Hızlı Stok Giriş</a></h1> </div>
+<div class="col-lg-12"><h1 class="page-header">Ürünler
+<a class="btn btn-primary btn-default" href="modify_stok.php" style="float: right;"><i class="fa fa-dropbox fa-lg"aria-hidden='true'></i> Hızlı Stok Giriş</a>
+<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal" style="float: right;"><i class="fa fa-assistive-listening-systems"></i> Stok Alarmları</button></h1> </div>
 <div class="col-lg-12">
 <div class="panel panel-default">
 <div class="panel-body">
@@ -64,16 +66,59 @@ error_reporting(0);
 		echo "</table></form>";
 		echo "</div>";
 		
-		((is_null($___mysqli_res = mysqli_close( $con ))) ? false : $___mysqli_res);
+		//((is_null($___mysqli_res = mysqli_close( $con ))) ? false : $___mysqli_res);
 	?>
-		
+
 </div>
 </div>
 </div>
 </div>
 </div>
 </div>
-		
+
+
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title"><i class="fa fa-assistive-listening-systems"></i> Stok Alarmları</h4>
+      </div>
+      <div class="modal-body">
+      	<?php
+      	$sql = "SELECT 35cgnproduct.name,35cgnproduct.stok,35cgnproductalarm.stoklimit FROM 35cgnproduct, 35cgnproductalarm WHERE 35cgnproduct.stok < 35cgnproductalarm.stoklimit AND 35cgnproduct.id=35cgnproductalarm.id";
+				$res = mysqli_query($GLOBALS["___mysqli_ston"],  $sql);
+				$bildirimvar=false;
+				while ( $row = mysqli_fetch_array($res) ) {
+					$bildirimvar=true;
+					echo '<p>Ürün Adı:'.$row['name'].'<br>Stok Adedi:'.$row['stok'].'<br>Stok Alarmınız:'.$row['stoklimit'].'</p>';
+				}
+				if(!$bildirimvar) echo '<p>Stok kayıtlarınızda tetiklenen bir alarm bulunmamaktadır.</p>'.$sql;
+      	?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Kapat</button>
+      </div>
+    </div>
+    <?php
+    if ($bildirimvar){
+    	echo '<script>
+			$(document).ready(function(){
+			    // Show the Modal on load
+			    $("#myModal").modal("show");
+			});
+			</script>';
+    }
+    ?>
+  </div>
+</div>
+
+
 </body>
 </html>
 <?php ob_flush(); ?>
